357622e478574840d3e5f84f2d01b313292f5934
finduraf <Filip.Findura@osc.cz>
2022-11-04 15:43:50 +0100

    feat: Stat screen

--- app/src/main/java/com/example/quizgame/Stats.kt
+++ app/src/main/java/com/example/quizgame/Stats.kt
@@ -3,9 +3,9 @@ package com.example.quizgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Stats : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_stats)
     }
 }
