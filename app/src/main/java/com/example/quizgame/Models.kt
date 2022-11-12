4f507639bc46224ed307f082b8b44ceac9ccdc3e
finduraf <Filip.Findura@osc.cz>
2022-11-12 13:28:09 +0100

    feat: Add data models

--- app/src/main/java/com/example/quizgame/Models.kt
+++ app/src/main/java/com/example/quizgame/Models.kt
@@ -4,3 +4,27 @@ class StatFeed(
     val name: String,
     val image: Int
 )
+
+class ResultList(
+    val results: ArrayList<ResultFeed>,
+    val response_code: Int
+)
+
+class QuizResult(
+    val result: String
+)
+
+class ResultFeed(
+    val category: String,
+    val type: String,
+    val difficulty: String,
+    val question: String,
+    val correct_answer: String,
+    val incorrect_answers: ArrayList<String>
+)
+
+class JoinedFeed(
+    val questions: ArrayList<String>,
+    val answers: ArrayList<ArrayList<String>>,
+    val answerOK: ArrayList<String>
+)
