d31cca24d7df263d4508af11e1cbb25a4c4551d2
finduraf <Filip.Findura@osc.cz>
2022-11-12 16:24:51 +0100

    feat: Unify start button

--- app/src/main/java/com/example/quizgame/MainActivity.kt
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        val startButton = findViewById<Button>(R.id.startButton)
+        val startButton = findViewById<ImageButton>(R.id.startButton)
         startButton.setOnClickListener {
            val moveTo = Intent(this, Stats::class.java)
            startActivity(moveTo)
--- app/src/main/java/com/example/quizgame/Stats.kt
+++ app/src/main/java/com/example/quizgame/Stats.kt
@@ -13,16 +13,16 @@ class Stats : AppCompatActivity() {
         supportActionBar?.hide()
 
         val allStats: ArrayList<StatFeed> = ArrayList()
-        allStats.add(StatFeed(name = "Total Score", image = R.drawable.icon_trophy))
-        allStats.add(StatFeed(name = "Test", image = R.drawable.icon_note))
+        allStats.add(StatFeed(name = "Trophies", image = R.drawable.icon_trophy))
+        allStats.add(StatFeed(name = "Categories", image = R.drawable.icon_note))
         allStats.add(StatFeed(name = "Previous Score", image = R.drawable.icon_calendar))
         allStats.add(StatFeed(name = "Time Taken", image = R.drawable.icon_clock))
 
         val grid = findViewById<GridView>(R.id.statusGridView)
         grid.adapter = StatAdapter(this, allStats)
 
-        val nxtButton = findViewById<ImageButton>(R.id.nextButton)
-        nxtButton.setOnClickListener {
+        val startButton = findViewById<ImageButton>(R.id.startButton)
+        startButton.setOnClickListener {
             val intent = Intent(this, Questions::class.java)
             startActivity(intent)
         }
